38e92696d80a45acab550f0ce05d701048db0133
Meljimm17 <jmeljimm@gmail.com>
2026-09-23 00:30:08 +0800

    first commit

---
+++ lib/crud_service.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CrudService {
+  final CollectionReference items =
+      FirebaseFirestore.instance.collection('items');
+
+  Future<void> addItem(String name, int quantity) {
+    return items.add({
+      'name' : name,
+      'quantity' : quantity,
+      'createdAt' : Timestamp.now(),
+    });
+  }
+
+  //Read
+  Stream <QuerySnapshot> getItems(){
+    return items.orderBy('createdAt', descending: true).snapshots();
+  }
+
+  //Update
+  Future <void> updateItem(String id, String name, int quantity){
+    return items.doc(id).update({
+      'name' : name,
+      'quantity' : quantity,
+    });
+  }
+
+  //Delete
+  Future <void> deleteItem(String id){
+    return items.doc(id).delete();
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,199 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:notes/crud_service.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  final CrudService crudService = CrudService();
+  final TextEditingController nameCtrl = TextEditingController();
+  final TextEditingController qtyCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: const Text('Firebase Gilay'),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.teal,
+        child: const Icon(Icons.add, color: Colors.white),
+        onPressed: () => openAddDialog(context),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: crudService.getItems(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final docs = snapshot.data!.docs;
+          if (docs.isEmpty){
+            return const Center(child: Text('No Items Found', style: TextStyle(fontSize: 18)));
+          }
+          return ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: docs.length,
+            itemBuilder: (context, index){
+              var item = docs[index];
+              return Card(
+                elevation: 3,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                margin: const EdgeInsets.symmetric(vertical: 6),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  title: Text(
+                    item['name'],
+                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text("Quantity ${item['quantity']}",
+                  style: const TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.edit, color: Colors.orange),
+                      onPressed: () => openEditDialog(context, item),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                      onPressed: () => _confirmDelete(context, item.id),
+                    ),
+                  ],
+                ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  void _confirmDelete(BuildContext context, String id){
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text("Delete Item"),
+        content: const Text("Are sure you want to delete this item?"),
+        actions: [
+          TextButton(
+            child: const Text("Delete", style: TextStyle(color: Colors.red)),
+            onPressed: (){
+              crudService.deleteItem(id);
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      )
+    );
+  }
+
+  void openAddDialog(BuildContext context){
+    nameCtrl.clear();
+    qtyCtrl.clear();
+
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text("Add Item"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameCtrl,
+              decoration: InputDecoration(
+                labelText: "Name",
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))
+              ),
+            ),
+            SizedBox(height: 12),
+            TextField(
+              controller: qtyCtrl,
+              decoration: InputDecoration(
+                labelText: "Quantity",
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            child: const Text('Cancel'),
+            onPressed: () => Navigator.pop(context),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.teal,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(8)),
+            ),
+            child: const Text("Save"),
+            onPressed: () async {
+              if (nameCtrl.text.isNotEmpty && qtyCtrl.text.isNotEmpty) {
+                await crudService.addItem(nameCtrl.text, int.parse(qtyCtrl.text));
+                if (context.mounted) {
+                  Navigator.pop(context);
+                }
+              }
+            },
+          ),
+        ],
+      )
+    );
+  }
+
+//EDIT UI
+void openEditDialog(BuildContext context, DocumentSnapshot item){
+  nameCtrl.text = item['name'];
+  qtyCtrl.text = item['quantity'].toString();
+
+  showDialog(
+    context: context,
+    builder: (_) => AlertDialog(
+      title: const Text("Edit Item"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: nameCtrl,
+            decoration: InputDecoration(
+              labelText: "name",
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+            ),
+          ),
+          const SizedBox(height: 12),
+          TextField(
+            controller: qtyCtrl,
+            decoration: InputDecoration(
+              labelText: "Quantity",
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          child: const Text("Cancel"),
+          onPressed: () => Navigator.pop(context),
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.orange,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          ),
+          child: const Text('Update'),
+          onPressed: () {
+            if (nameCtrl.text.isNotEmpty && qtyCtrl.text.isNotEmpty) {
+              crudService.updateItem(item.id, nameCtrl.text, int.parse(qtyCtrl.text));
+              Navigator.pop(context);
+            }
+          },
+        ),
+      ],
+    ),
+  );
+}
+}
